6d285fa44cf3009b4c4961f68bfdfb2c8fabf2cf
Paul Sparks <psparks1225@gmail.com>
2024-02-10 05:00:28 -0500

    clean up code, optimize for reusability

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,50 +1,45 @@
+class_name Enemy
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
 @export var speed = 10
 @export var playerPath = NodePath()
 
-@onready var _sprite_2d = $Pivot/PlayerSprite
+@onready var _sprite_3d = $Pivot/PlayerSprite
 @onready var _animation_tree: AnimationTree = $Pivot/AnimationTree
 @onready var _player = get_node(playerPath)
 
 var target_velocity = Vector3.ZERO
 var distance = Vector3.ZERO
-var isClose: bool = false
-
-func flip_sprites_h(value: bool):
-	_sprite_2d.set_flip_h(value)
+var is_colliding: bool = false
 
 func _physics_process(_delta):
-	# We create a local variable to store the input direction.
+	movement_logic()
+	update_animation_parameters()
+	
+func movement_logic():
 	var direction = Vector3.ZERO
 
 	distance = (_player.global_position - global_position)
-	
-	isClose = distance.abs().x < 9 and distance.abs().z < 5
 	
 	if distance != Vector3.ZERO:
 		direction = distance.normalized()
 		
 	if direction.x > 0:
-		flip_sprites_h(false)
+		_sprite_3d.set_flip_h(false)
 	else:
-		flip_sprites_h(true)
+		_sprite_3d.set_flip_h(true)
 
-	if !isClose:
+	if !is_colliding:
 		target_velocity.x = direction.x * speed
 		target_velocity.z = direction.z * speed
 	else:
 		target_velocity = Vector3.ZERO
 
-	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
-	
-	update_animation_parameters()
 
 func update_animation_parameters():
-		if isClose:
+		if is_colliding:
 			_animation_tree["parameters/conditions/notMoving"] = true
 			_animation_tree["parameters/conditions/isMoving"] = false
 			
@@ -56,3 +51,11 @@ func update_animation_parameters():
 			
 			_animation_tree["parameters/conditions/attacking"] = false
 			_animation_tree["parameters/conditions/notAttacking"] = true
+
+func _on_area_3d_area_entered(area):
+	if area.is_in_group("player_trigger"):
+		is_colliding = true
+
+func _on_area_3d_area_exited(area):
+	if area.is_in_group("player_trigger"):
+		is_colliding = false
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,23 +1,20 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
 @export var speed = 30
 
-@onready var _sprite_2d = $Pivot/PlayerSprite
+@onready var _sprite_3d = $Pivot/PlayerSprite
 @onready var _animation_tree: AnimationTree = $Pivot/AnimationTree
 @onready var _on_hand_sprite = $Pivot/OnHandSprite
 
 var target_velocity = Vector3.ZERO
 
 func flip_sprites_h(value: bool):
-	_sprite_2d.set_flip_h(value)
+	_sprite_3d.set_flip_h(value)
 	_on_hand_sprite.set_flip_h(value)
 
 func _physics_process(_delta):
-	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
-	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 		flip_sprites_h(false)
@@ -25,8 +22,6 @@ func _physics_process(_delta):
 		direction.x -= 1
 		flip_sprites_h(true)
 	if Input.is_action_pressed("move_down"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
 		direction.z += 1
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
@@ -34,11 +29,9 @@ func _physics_process(_delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
-	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 	
-	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
 	
