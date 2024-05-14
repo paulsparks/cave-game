9ad2d4be7d130def4af0848299b176ada12085b0
Dylan Van Eaton <dylanvaneaton@gmail.com>
2024-05-13 23:05:07 -0400

    knockback sorta works

---
+++ Knockback.gd
@@ -0,0 +1,5 @@
+extends Object
+class_name Knockback
+
+var horizontal: float
+var vertical: float
--- scripts/Attack.gd
+++ scripts/Attack.gd
@@ -2,3 +2,5 @@ extends Object
 class_name Attack
 
 var attack_damage: float
+var knockback: Knockback
+var attack_position: Vector3
